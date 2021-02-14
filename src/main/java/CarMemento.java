9f59f446da7402fad1e5dc77a097628be4057606
alt277 <altima875@gmail.com>
2021-02-14 11:26:38 +0300

    Initial commit

---
+++ src/main/java/CarMemento.java
@@ -0,0 +1,30 @@
+public class CarMemento {
+    private String model;
+    private Brand brand;
+    private Exterior exterior;
+    private Interior interior;
+
+    public CarMemento() {
+    }
+
+    public CarMemento(Exterior exterior, Interior interior) {
+        this.exterior = exterior;
+        this.interior = interior;
+    }
+
+    public Exterior getExterior() {
+        return exterior;
+    }
+
+    public void setExterior(Exterior exterior) {
+        this.exterior = exterior;
+    }
+
+    public Interior getInterior() {
+        return interior;
+    }
+
+    public void setInterior(Interior interior) {
+        this.interior = interior;
+    }
+}
